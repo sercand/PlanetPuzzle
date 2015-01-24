2f81738020056001c7b0c7bea28144bb5ea6cab3
Sercan Değirmenci <degirmencisercan@gmail.com>
2015-01-24 15:00:51 +0200

    add missing puzzlePiece.cs

---
+++ Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,29 @@
+﻿// #
+// # Created by Sercan Degirmenci on 2015.01.23
+// #
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Temp
+{
+    public class PuzzlePiece
+    {
+        public int Length
+        {
+            get { return Vertices.Count; }
+        }
+
+        public List<Vector3> Vertices;
+        public List<int> Triangenles;
+        public List<Edge> Edges;
+    }
+
+    public class Edge
+    {
+        public Vector3 v1;
+        public Vector3 v2;
+        public PuzzlePiece n1;
+        public PuzzlePiece n2;
+    }
+}
